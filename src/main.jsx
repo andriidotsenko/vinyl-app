172fc1279b67c58a42f25037bec1bb958d7502bf
andriidotsenko <andrui.dots@gmail.com>
2024-02-09 11:27:11 +0200

    add app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { App } from "./App";
+import { App } from "./App.jsx";
 
 const appElement = document.getElementById("app");
 const root = createRoot(appElement);
