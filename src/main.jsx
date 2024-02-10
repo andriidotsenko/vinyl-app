bc8c8123fa3f101c47376b4132a7b7ef440eb89b
andriidotsenko <andrui.dots@gmail.com>
2024-02-10 13:01:07 +0200

    add import/resolver node extensions js jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { App } from "./App.jsx";
+import { App } from "./App";
 
 const appElement = document.getElementById("app");
 const root = createRoot(appElement);
