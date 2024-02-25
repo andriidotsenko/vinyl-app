d7123c461091fd90519f322ef2e6449a142ae5a7
andriidotsenko <andrui.dots@gmail.com>
2024-02-25 22:40:16 +0200

    task 3.02 fix startup bug

--- tasks/3.02-async-state/components/IconButton.jsx
+++ tasks/3.02-async-state/components/IconButton.jsx
@@ -2,10 +2,9 @@ import clsx from "clsx";
 
 import styles from "./IconButton.module.css";
 
-function IconButton({ icon, white, dark, small, ...props }, ref) {
+function IconButton({ icon, white, dark, small, ...props }) {
   return (
     <button
-      ref={ref}
       className={clsx(styles.root, {
         [styles.isWhite]: white,
         [styles.isDark]: dark,
