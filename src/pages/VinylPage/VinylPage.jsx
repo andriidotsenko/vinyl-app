2d9101ab48d18aa3ecab52ee49dea0017a945796
Andrii Dotsenko <andrui.dots@gmail.com>
2024-03-18 21:44:50 +0200

    add OG and fix

--- src/pages/VinylPage/VinylPage.jsx
+++ src/pages/VinylPage/VinylPage.jsx
@@ -1,7 +1,6 @@
+import { Helmet } from "react-helmet-async";
 import VinylCard from "../../components/VinylCard/VinylCard";
 import { useVinylById } from "../../hooks/useVinylById";
-import styles from "./VinylPage.module.css";
-
 import { useOutletContext, useParams } from "react-router-dom";
 
 export function VinylPage() {
@@ -11,13 +10,23 @@ export function VinylPage() {
     handleCollectionToggle,
     handleFavoritesToggle,
   } = useOutletContext();
-
   const { vinylId } = useParams();
-
   const card = useVinylById(Number(vinylId));
 
+  const ogTitle = card ? `${card.name} - ${card.artist}` : "Vinyl Page";
+  const ogDescription = card ? card.description : "Description for Vinyl Page";
+  const ogImageUrl = card ? card.imageUrl : "/default-image.jpg";
+
   return (
-    <div className={styles.main}>
+    <>
+      <Helmet>
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:type" content="website" />
+        <title>{ogTitle}</title>
+      </Helmet>
+
       <VinylCard
         card={card}
         inCollection={collectionList.includes(card.id)}
@@ -25,6 +34,6 @@ export function VinylPage() {
         onClickInCollection={() => handleCollectionToggle(card.id)}
         onClickInFavorites={() => handleFavoritesToggle(card.id)}
       />
-    </div>
+    </>
   );
 }
