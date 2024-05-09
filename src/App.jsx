8aa2e0785387821465fc0f702ae0948243080d48
Andrii Dotsenko <andrui.dots@gmail.com>
2024-05-09 12:41:27 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { Suspense } from "react";
 import Header from "./components/Header/Header.jsx";
-import { useCollection } from "./hooks/useCollection.js";
+import { useCollectionNotes } from "./hooks/useCollection.js";
 import { useFavorites } from "./hooks/useFavorites.js";
 import { Outlet } from "react-router-dom";
 import { Loader } from "./components/Loader/Loader.jsx";
-import { useNote } from "./hooks/useNotes.js";
 
 import { Notifications } from "./components/Notifications/Notifications.jsx";
 import { useNotifications } from "./hooks/useNotifications.js";
 
 export function App() {
   const [addNotification, notifications] = useNotifications();
+
   const {
     collectionList,
     handleAddedToCollection,
     handleRemovedFromCollection,
-  } = useCollection(addNotification);
+    addNote,
+    noteList,
+  } = useCollectionNotes(addNotification);
+
   const { favoritesList, handleFavoritesToggle } =
     useFavorites(addNotification);
-  const { noteList, addNote } = useNote();
+
   return (
     <>
       <Header
--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,6 +1,6 @@
 import { useEffect, useReducer } from "react";
 
-const collectionReducer = (state, action) => {
+const collectionNoteReducer = (state, action) => {
   let newNoteList;
   switch (action.type) {
     case "ADDED_TO_COLLECTION":
@@ -35,26 +35,25 @@ const collectionReducer = (state, action) => {
   }
 };
 
-export const useCollection = (addNotification) => {
+const resetNote = "";
+export const useCollectionNotes = (addNotification) => {
   const initialState = {
     collectionList: localStorage.getItem("collectionList")
       ? JSON.parse(localStorage.getItem("collectionList"))
       : [],
     noteList: JSON.parse(localStorage.getItem("noteList")) || {},
   };
-  const [state, dispatch] = useReducer(collectionReducer, initialState);
+  const [state, dispatch] = useReducer(collectionNoteReducer, initialState);
 
   const handleAddedToCollection = (vinyl, id) => {
-    const resetNote = "";
+    dispatch({ type: "ADDED_NOTE", payload: { id, note: resetNote } });
     dispatch({ type: "ADDED_TO_COLLECTION", payload: { vinyl } });
     addNotification(`"${vinyl.title}" added to collection`);
-    dispatch({ type: "ADDED_NOTE", payload: { id, note: resetNote } });
   };
 
   const handleRemovedFromCollection = (vinyl, id) => {
     dispatch({ type: "REMOVED_FROM_COLLECTION", payload: { vinyl } });
     dispatch({ type: "REMOVED_NOTE", payload: { id } });
-
     addNotification(`"${vinyl.title}" removed from collection`);
   };
 
@@ -72,7 +71,7 @@ export const useCollection = (addNotification) => {
       JSON.stringify(state.collectionList)
     );
     localStorage.setItem("noteList", JSON.stringify(state.noteList));
-  }, [state.collectionList, state.noteList, removeNote, addNote]);
+  }, [state.collectionList, state.noteList]);
 
   return {
     collectionList: state.collectionList,
