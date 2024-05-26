2d03f1c799a1b00abd26b898978c0d20bf8449bf
Andrii Dotsenko <andrui.dots@gmail.com>
2024-05-26 22:02:47 +0300

    fix public

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
+const splitHomepage = packageJson.homepage.split("/");
+const baseUrl = splitHomepage[splitHomepage.length - 2];
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    assetsDir:
+      // eslint-disable-next-line no-undef
+      process.env.NODE_ENV === "production" ? baseUrl : "./",
+  },
   plugins: [
     react({
       jsxImportSource: "@welldone-software/why-did-you-render",
